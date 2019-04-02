036fd78f6ccdcc99b0bce5a5d5abe2d11c70ef8c
Kevin Liu <kevin.liu@Kevin-Liu.local>
2019-04-02 14:21:43 -0400

    sfdj

--- goal12.js
+++ goal12.js
@@ -1,14 +1,36 @@
 
-var staticUrl = 'https://raw.githubusercontent.com/Kliu2022/Kliu2022.github.io/master/Creating%20the%20Solution/data/fao_losses_2013_na.json';
-var staticUrl2 = 'https://raw.githubusercontent.com/Kliu2022/Kliu2022.github.io/master/Creating%20the%20Solution/data/faostat_production_2013_na.json';
+var staticUrl = 'https://raw.githubusercontent.com/Kliu2022/Kliu2022.github.io/master/data/fao_losses_2013_na.json';
+var staticUrl2 = 'https://raw.githubusercontent.com/Kliu2022/Kliu2022.github.io/master/faostat_production_2013_na.json';
+
+
 
 
 
 $.getJSON(staticUrl, function(data) {
 
-    console.log(data)//.
+    len = data.length;
+    var loss = new Array(len);
+    for(i = 0; i < len; i = i + 1){
+        loss[i] = data[i];
+    }
+  
+});
 
-    iw = window.innerWidth - 300;
+
+$.getJSON(staticUrl2, function(data) {
+
+    len = data.length;
+    var prod = new Array(len);
+    for(i = 0; i < len; i = i + 1){
+        prod[i] = data[i];
+    }
+  
+});
+
+Console.log(prod);
+Console.log(loss);
+
+/*iw = window.innerWidth - 300;
     foodData = data[0].Value;
     val = parseInt(foodData);
     largest = val;
@@ -16,44 +38,30 @@ $.getJSON(staticUrl, function(data) {
 
     for (i = 0; i < len; i = i + 1) {
 
-    	val = data[i].Value;
+        val = data[i].Value;
         
         if (isNaN(val) === false) {
             largest = Math.max(largest,val);
         }
     }
     
-		col = ["lightblue"];
+        col = ["lightblue"];*/
 
-		
+val = data[i].Value; 
 
+console.log(val);
+vals = val/largest*iw;
 
-    for (i = 0; i < len; i = i + 1) {
-    	
+var div = document.createElement("div");
+document.getElementById("datasection").appendChild(div);
+div.setAttribute("id","div"+i);  
+div.style.width = vals+"px";
+document.getElementById("div"+i).style.backgroundColor = col[0];
 
-		val = data[i].Value; 
-
-		console.log(val);
-        vals = val/largest*iw;
-
-        var div = document.createElement("div");
-        document.getElementById("datasection").appendChild(div);
-        div.setAttribute("id","div"+i);  
-        div.style.width = vals+"px";
-		document.getElementById("div"+i).style.backgroundColor = col[0];
-
-        var p = document.createElement("p");        
-        document.getElementById("div"+i).appendChild(p);                
-        p.setAttribute("id","p"+i);
-        p.setAttribute("font-family", "Raleway, Arial, Helvetica, sans-serif");
-        country = data[i].Item;                
-        p.innerHTML = "<pre>"+country+": "+val+"</pre>";
-
-
-    }
-
-   
-
-  
-});
+var p = document.createElement("p");        
+document.getElementById("div"+i).appendChild(p);                
+p.setAttribute("id","p"+i);
+p.setAttribute("font-family", "Raleway, Arial, Helvetica, sans-serif");
+country = data[i].Item;                
+p.innerHTML = "<pre>"+country+": "+val+"</pre>";
 
